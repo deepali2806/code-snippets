8f59922bdc44eb81948fcb091db067d06d6d0932
Deepali <deepaliande.1@gmail.com>
2021-12-18 22:03:30 +0530

    Added locks around lseek & read

--- DomainsTest/countLines.ml
+++ DomainsTest/countLines.ml
@@ -4,9 +4,11 @@ let buffer_size = 200
 let num_domains = 4
 
 let rec countInBuffer buffer noOfBytes init = 
-  if noOfBytes = 0
+  if noOfBytes = 0 && (Bytes.get buffer noOfBytes) = '\n'
   then
-    init
+    init + 1
+  else if noOfBytes = 0
+    then init 
   else
     begin
     if (Bytes.get buffer noOfBytes) = '\n' then
@@ -19,15 +21,19 @@ let parallel_line_count pool filename =
   let fd_in = openfile filename [O_RDONLY] 0 in
   let st = fstat fd_in in 
   let i_n = (st.st_size / buffer_size) in
-
+  let m = Mutex.create () in
   let v = Domainslib.Task.parallel_for_reduce ~start:0 ~finish:(i_n) ~body:(fun i ->
         let offset = i*buffer_size in    
         let buffer = Bytes.create buffer_size in
+
+        Mutex.lock m;
         let _ = lseek fd_in (offset) SEEK_SET in
         let noOfBytes = read fd_in buffer 0 buffer_size in
+        Mutex.unlock m;
 
         let cnt = countInBuffer buffer (noOfBytes-1) 0 in
-        Printf.printf "\nLength of cnt %d" cnt; cnt
+        (* Printf.printf "\nLength of cnt %d" cnt;  *)
+        cnt
   ) pool (+) 0 in
     Printf.printf "Count of Lines %d" v
 
