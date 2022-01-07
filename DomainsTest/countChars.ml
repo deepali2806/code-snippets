d4c56e603c7676738b9e3bc8d0b4d259a99a236e
Deepali <deepaliande.1@gmail.com>
2022-01-07 12:07:36 +0530

    Count number characters in a file

---
+++ DomainsTest/countChars.ml
@@ -0,0 +1,71 @@
+open Unix;;
+
+let num_domains = try int_of_string Sys.argv.(1) with _ -> 1
+let buffer_size = try int_of_string Sys.argv.(2) with _ ->  4096
+
+(* 
+let rec countInBuffer buffer noOfBytes init = 
+  if noOfBytes = 0 && (Bytes.get buffer noOfBytes) = '\n'
+  then
+    init + 1
+  else
+   if noOfBytes = 0
+    then init 
+  else
+    begin
+    if (Bytes.get buffer noOfBytes) = '\n' then
+      countInBuffer buffer (noOfBytes-1) (init + 1)
+    else
+      countInBuffer buffer (noOfBytes-1) init
+    end *)
+
+
+(* 
+let countInBuffer buffer noOfBytes = 
+let total = ref 0 in
+    for i = 0 to noOfBytes-1
+    do
+       if (Bytes.get buffer i) = '\n' then
+          total := !total + 1
+    done;
+    !total *)
+
+
+let rec countInBuffer buffer noOfBytes init = 
+  if noOfBytes = 0
+    then init+1
+  else
+      countInBuffer buffer (noOfBytes-1) (init + 1)
+     
+let parallel_line_count pool filename =
+  let fd_in = openfile filename [O_RDONLY] 0 in
+  let st = fstat fd_in in 
+  let i_n = (st.st_size / buffer_size) in
+  let k1 = Domain.DLS.new_key (fun () -> openfile filename [O_RDONLY] 0) in
+  let k2 = Domain.DLS.new_key (fun () -> Bytes.create buffer_size) in
+  let v = Domainslib.Task.parallel_for_reduce ~start:0 ~finish:(i_n) ~body:(fun i ->
+
+         let fd = Domain.DLS.get k1 in
+        (* let fd = openfile filename [O_RDONLY] 0 in  *)
+        let offset = i*buffer_size in    
+        let buffer = Domain.DLS.get k2 in
+       let _ = try lseek fd (offset) SEEK_SET with _ -> -1 in
+        let noOfBytes = read fd buffer 0 buffer_size in
+
+        let cnt = countInBuffer buffer (noOfBytes-1) 0 in
+        (* let cnt = i in *)
+        (* Printf.printf "\nLength of cnt %d" cnt;  *)
+        (* close fd; *)
+        cnt
+  ) pool (+) 0 in
+    Printf.printf "Count of Lines %d" v
+    (* close fd_in *)
+
+let main =
+    let filename = Sys.argv.(3) in   
+    let pool = Domainslib.Task.setup_pool ~num_additional_domains:(num_domains - 1) () in
+    let start = Unix.gettimeofday () in
+    let _ = parallel_line_count pool filename in
+    let stop = Unix.gettimeofday () in
+    Domainslib.Task.teardown_pool pool;
+    Printf.printf "\nDone \nTime %f\n" ( stop -. start)
---
+++ EioTest/countCharsEio.ml
@@ -0,0 +1,42 @@
+open Eio.Std
+open Eio_linux
+module Int63 = Optint.Int63
+let blkSize = try int_of_string Sys.argv.(1) with _ -> 4096
+
+
+let rec countInBuffer buffer noOfBytes init = 
+  if noOfBytes = 0
+    then init+1
+  else
+      countInBuffer buffer (noOfBytes-1) (init + 1)
+
+let () = 
+    run @@ fun _env ->
+    Switch.run @@ fun sw ->
+    let fd = Unix.handle_unix_error (openfile ~sw (Sys.argv.(2)) Unix.[O_RDONLY]) 0 in 
+    let st = fstat fd in
+    let no_fibres = (st.st_size / blkSize) in
+    let clock = Eio.Stdenv.clock _env in
+    let count = Atomic.make 0 in 
+    let t1 = Eio.Time.now clock in
+    (* traceln "The time is now %f" t1; *)
+    Switch.run (fun sw ->
+        for i = 0 to no_fibres
+        do
+             Fibre.fork ~sw
+            (fun () ->  let buf = alloc () in
+                        let off = i*blkSize in
+                        (* let _ = Unix.lseek (Eio_linux.FD.to_unix fd) off SEEK_SET in *)
+                        let noOfBytes = read_upto fd ~file_offset:(Int63.of_int off) buf blkSize in 
+                        let buffer = Uring.Region.to_cstruct ~len:noOfBytes buf in
+                        let cnt = countInBuffer buffer (noOfBytes-1) 0 in
+                        let _ = Atomic.fetch_and_add count cnt in 
+                        (* traceln "\nCount in each fibre %d currentvalue %d " cnt currentVal; *)
+                        (* traceln "Offset and number of fibre %d, %d" off i; *)
+		                free buf
+            );
+        done	
+    );
+    let t2 = Eio.Time.now clock in
+    traceln "Final count is %d \nTime Difference %f" (Atomic.get count) (t2-.t1);
+    traceln "\nSwitch is finished"
